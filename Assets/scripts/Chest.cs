795cd1b8f351e1eb2072e7b626adea83f725fa76
callumfay <callumfay_16@hotmail.co.uk>
2018-04-03 20:48:40 +0100

    Added a chest

--- Assets/scripts/Chest.cs
+++ Assets/scripts/Chest.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace TextRPG { 
+    public class Chest : MonoBehaviour
+    {
+        public string Item { get; set; }
+        public int Gold { get; set; }
+        public bool Trap { get; set; }
+        public bool Heal { get; set; }
+        public Enemy Enemy { get; set; }
+
+        public Chest()
+        {
+            if (Random.Range(0, 7) == 2)
+            {
+                Trap = true;
+            }
+            else if (Random.Range(0, 5) == 2)
+            {
+                Heal = true;
+            }
+            else if (Random.Range(0, 5) == 2)
+            {
+                Enemy = EnemyDatabase.Instance.Enemies[Random.Range(0, EnemyDatabase.Instance.Enemies.Count)];
+            }
+            else
+            {
+                int itemToAdd = Random.Range(0, ItemDatabase.Instance.Items.Count);
+                Item = ItemDatabase.Instance.Items[itemToAdd];
+                Gold = Random.Range(20, 200);
+            }
+        }
+    }
 }
